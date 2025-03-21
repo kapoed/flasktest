b731d3e599cfc58ac2a395eeb2a850dab1970557
kapoed <ilya.karto@gmail.com>
2025-03-21 19:32:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -176,5 +176,28 @@ def astronaut_selection():
         print(request.form['sex'])
         return "Форма отправлена"
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f'''<!doctype html>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Колонизация</title>
+                    </head>
+                    <body>
+                        <h1>Моё предложение: {planet_name}</h1> 
+                        <h3>Эта планета близка к земле;</h3> 
+                        <div class="p-3 mb-2 bg-secondary text-black">Она круглая</div>
+                        <div class="p-3 mb-2 bg-success text-green">На ней есть {planet_name}иане</div>
+                        <div class="p-3 mb-2 bg-info text-cyan">На неё светит солнце</div>
+                        <div class="p-3 mb-2 bg-warning text-yellow">Наконец, она просто красива!</div>
+                    </body>
+                </html>'''
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
