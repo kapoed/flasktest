ae4f7a7976043187a534e97c34adf99a6b0bc1db
kapoed <ilya.karto@gmail.com>
2025-03-22 17:39:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/index/<title>')
 def index(title):
     return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, img_science=url_for('static', filename='img/right_arrow.jpg'),
+                           img_engineering=url_for('static', filename='img/left_arrow.jpg'))
 
 
 @app.route('/promotion')
