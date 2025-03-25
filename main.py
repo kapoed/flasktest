26b183ef627e756846391be676d5be9abae1cf5e
kapoed <ilya.karto@gmail.com>
2025-03-25 19:08:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 @app.route('/index/<title>')
@@ -15,6 +25,13 @@ def training(prof):
 @app.route('/list_prof/<list>')
 def list_prof(list):
     return render_template('list_prof.html', list=list)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 @app.route('/promotion')
 def promotion():
@@ -65,7 +82,7 @@ def promotion_image():
                 </html>'''
 
 
-@app.route('/astronaut_selection')
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
 def astronaut_selection():
     if request.method == 'GET':
         return f'''<!doctype html>
